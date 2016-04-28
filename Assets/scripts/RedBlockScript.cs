3100058c5245de0639e371ee8c61bfcbfde5c2db
tjaydkSchool <dennis.michael@jubii.dk>
2016-04-28 15:54:45 +0200

    Ebbes scaripts

---
+++ Assets/scripts/RedBlockScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedBlockScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collision col){
+		if (col.gameObject.name == "RollerBall") {
+			this.gameObject.AddComponent<RigidBody>().useGravity = true;
+			Destroy (this.gameObject, 3f);
+		}
+	}
+}
---
+++ Assets/scripts/UnstableBlockScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnstableBlockSrcipt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collision col) {
+		if (col.gameObject.name == "RollerBall") {
+			StartCoroutine (destroyBlock());
+		}
+	}
+
+	IEnumerable destroyBlock() {
+		yield return new WaitForSeconds (0.5f);
+		this.gameObject.AddComponent<RigidBody>().useGravity = true;
+		Destroy (this.gameObject, 3f);
+	}
+
+}
