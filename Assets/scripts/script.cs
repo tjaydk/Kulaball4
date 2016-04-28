174cc516d4b8a9f49715ea5dc7ff5779bf026106
Ebski <ebbe_vig@hotmail.com>
2016-04-28 18:09:39 +0200

    New scene

--- Assets/scripts/script.cs
+++ Assets/scripts/script.cs
@@ -6,18 +6,16 @@ public class script : MonoBehaviour {
 	public GameObject system;
 
 
-	public float jumpPower;
-	public float movePower;
-
 
 	//Advantages 
-	public bool ballSpeed = false;
+	public bool ballSpeedFaster = false;
 	public bool ballJumpHigher = false;
-	public bool ballJumpLower = false;
 	public bool scaledLarger = false;
 
 	//Disadvantages
-
+	public bool scaledSmaller = false;
+	public bool ballJumpLower = false;
+	public bool ballSpeedSlower = false;
 
 	private bool isActive = false;
 	private int interval = 1;
@@ -44,11 +42,16 @@ public class script : MonoBehaviour {
 				ball.GetComponent<Ball> ().m_JumpPower = 10;
 			}if (ballJumpLower == true) {
 				ball.GetComponent<Ball>().m_JumpPower = 0.2f;
-			}if (ballSpeed == true) {
+			}if (ballSpeedFaster == true) {
 				ball.GetComponent<Ball> ().m_MovePower = 20;
 			}if (scaledLarger == true) {
 				ball.transform.localScale += new Vector3 (2, 2, 2);
-			}
+			} if (scaledSmaller == true) {
+				ball.transform.localScale -= new Vector3 (0.5f, 0.5f, 0.5f);
+			}if (ballSpeedSlower == true) {
+				ball.GetComponent<Ball> ().m_MovePower = 0;
+				ball.GetComponent<Ball> ().m_MaxAngularVelocity = 2;
+			};
 			isActive = true;
 			InvokeRepeating ("randomMethod", 0, 1.0f);
 		}
@@ -61,7 +64,12 @@ public class script : MonoBehaviour {
 		} else if (nextTime == 5 && isActive == true) {
 			ball.GetComponent<Ball>().m_JumpPower = 1;
 			ball.GetComponent<Ball>().m_MovePower = 3;
-			ball.transform.localScale -= new Vector3 (2, 2, 2);
+			if (scaledLarger == true) {
+				ball.transform.localScale -= new Vector3 (2, 2, 2);
+			}
+			if (scaledSmaller) {
+				ball.transform.localScale += new Vector3 (0.5f, 0.5f, 0.5f);
+			}
 			isActive = false;
 			CancelInvoke ();
 
